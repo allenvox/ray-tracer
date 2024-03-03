fcb7f4effa1aea7a5e90fa2fdf62374f55e01ece
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-03-03 11:53:21 +0700

    Format code

--- src/raytracer/main.cpp
+++ src/raytracer/main.cpp
@@ -5,16 +5,16 @@
 #include "sphere.h"
 
 int main() {
-    hittable_list world;
+  hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    camera cam;
+  camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
-    cam.samples_per_pixel = 100;
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
+  cam.samples_per_pixel = 100;
 
-    cam.render(world);
+  cam.render(world);
 }
