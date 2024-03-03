ee6aca5342df30b6c4555a08f2ec03ff66564c13
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-03-04 01:39:34 +0700

    Add object materials header & refactor some headers & main program file to work with it

--- external/hittable.hpp
+++ external/hittable.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include "rtweekend.hpp"
+
 #include "ray.hpp"
+
+class material;
 
 class hit_record {
 public:
   point3 p;
   vec3 normal;
+  shared_ptr<material> mat;
   double t;
   bool front_face;
   void set_face_normal(const ray &r, const vec3 &outward_normal) {
