4e6920c188e014feb3251848b2c5e4fe0d8c9fc9
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-03-05 09:42:40 +0700

    Format code

--- src/include/vec3.hpp
+++ src/include/vec3.hpp
@@ -135,7 +135,7 @@ inline vec3 random_in_unit_disk() {
   while (true) {
     auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
     if (p.length_squared() < 1) {
-        return p;
+      return p;
     }
   }
 }
